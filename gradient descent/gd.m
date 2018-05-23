ab65b48fc423c3c82467ad5d06bd56d592d4a0bb
Caishu007 <caishu@njupt.edu.cn>
2018-05-23 10:36:21 +0800

    gradient descent\gd.m
    增加边界约束条件
        x = boundConstr(x,paras);

--- gradient descent/gd.m
+++ gradient descent/gd.m
@@ -1,5 +1,5 @@
 function x = gd(x0,paras,eps,maxIterNum,plotfig)
-% gradient descent method
+% gradient descent method to minimize fun(x)
 % x0: initial point
 % paras: parameters in function
 % eps: stop bound
@@ -9,7 +9,7 @@ if isempty(eps)
     eps = 1e-8;
 end
 if isempty(maxIterNum)
-    maxIterNum = 1e3;
+    maxIterNum = 1000;
 end
 if isempty(plotfig)
     plotfig = 1;
@@ -25,6 +25,8 @@ for it = 1:maxIterNum
     s = armijo_step(x,paras,d,grad);
     % renew x
     x = x + s*d;
+    % constraint
+    x = boundConstr(x,paras);
     % objs
     if plotfig == 1
         objs(it) = fun(x,paras);
@@ -36,7 +38,7 @@ for it = 1:maxIterNum
 end
 
 if plotfig == 1
-    figure
+    figure(999)
     plot(objs); grid on; hold on;
 end
 end
@@ -45,7 +47,7 @@ function s = armijo_step(x,paras,d,grad)
     sigma = 0.1;   %门限(<1)，通常较小
     beta = 0.5;   %缩减速度(<1)
     
-    s = 1;
+    s = 2;
     while fun(x,paras) + sigma*s*grad'*d  < fun(x+s*d,paras)   % new fun is not small enough
         s = s*beta;
     end
